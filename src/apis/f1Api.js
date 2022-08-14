509f532f8f9d139e4d758ae16c40abb08ea4da87
reinhartbuit <buit.reinhart@gmail.com>
2022-08-14 21:11:58 +0200

    PlayAround with API

---
+++ src/apis/f1Api.js
@@ -0,0 +1,3 @@
+import axios from 'axios';
+
+export default axios.create({ baseURL: 'http://ergast.com/api/f1/' });
--- src/index.js
+++ src/index.js
@@ -1,17 +1,24 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import f1Api from './apis/f1Api';
+import { XMLParser } from 'fast-xml-parser';
+
+const App = () => {
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await f1Api.get('/2022/drivers');
+      const parser = new XMLParser({
+        ignoreAttributes: false,
+        attributeNamePrefix: '__',
+      });
+      const { MRData } = parser.parse(response.data);
+      console.log(MRData.DriverTable.Driver[0]);
+    };
+    fetchData().catch(console.error);
+  });
+
+  return <div>App</div>;
+};
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+root.render(<App />);
