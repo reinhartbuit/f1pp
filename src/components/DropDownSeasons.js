2400930f2078ad5dc734480882bd3604aba56209
reinhartbuit <buit.reinhart@gmail.com>
2022-08-16 21:58:27 +0200

    Use Material UI

--- src/components/DropDownSeasons.js
+++ src/components/DropDownSeasons.js
@@ -1,9 +1,13 @@
 import React, { useEffect, useState } from 'react';
 import apiSports from '../apis/apiSports';
-import DatePicker from 'react-datepicker';
-import 'react-datepicker/dist/react-datepicker.css';
 
-const DropDownSeasons = ({ onSeasonSelect }) => {
+import TextField from '@mui/material/TextField';
+import { AdapterDateFns } from '@mui/x-date-pickers/AdapterDateFns';
+import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
+import { DatePicker } from '@mui/x-date-pickers/DatePicker';
+import moment from 'moment';
+
+const DropDownSeasons = ({ selectedSeason, onSeasonSelect }) => {
   const [minValue, setMinValue] = useState(new Date());
   const [maxValue, setMaxValue] = useState(new Date());
   const [startDate, setStartDate] = useState(new Date());
@@ -26,15 +30,19 @@ const DropDownSeasons = ({ onSeasonSelect }) => {
   };
 
   return (
-    <DatePicker
-      selected={startDate}
-      onChange={onDateChange}
-      showYearPicker
-      dateFormat={'yyyy'}
-      minDate={new Date(`${minValue}-01-01`)}
-      maxDate={new Date(`${maxValue}-01-01`)}
-      onSeasonSelect={onSeasonSelect}
-    ></DatePicker>
+    <LocalizationProvider dateAdapter={AdapterDateFns}>
+      <DatePicker
+        views={['year']}
+        label="Select Season"
+        value={startDate}
+        minDate={moment(`01-01-${minValue}`, 'DD-MM-YYYY').toDate()}
+        maxDate={moment(`01-01-${maxValue}`, 'DD-MM-YYYY').toDate()}
+        onChange={(newValue) => {
+          onDateChange(newValue);
+        }}
+        renderInput={(params) => <TextField {...params} helperText={null} />}
+      />
+    </LocalizationProvider>
   );
 };
 
